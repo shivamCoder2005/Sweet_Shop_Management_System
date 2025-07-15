4b15aec20d1296cd8b066fd629f912a45dcd183a
makwanashivam2005 <makwanashivam1709@gmail.com>
2025-07-15 22:36:57 +0530

    init project setup sucessfully

---
+++ Backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(3000, () => {
+  console.log("Server is listening on port ", port);
+});
+
+app.get("/", (req, res) => {
+  res.send("All Good");
+});
